6b3d0f4314b830abce797f6fcb9d9b4f19e73890
Radhwane Chebaane <radcheb@gmail.com>
2014-09-07 13:31:34 +0200

    plot 2

--- plot2.R
+++ plot2.R
@@ -4,7 +4,6 @@ data$Time <- strptime(paste(data$Date,data$Time,sep = ","),format="%d/%m/%Y,%H:%
 data[,'Date'] <- as.Date(data[,'Date'],format='%d/%m/%Y')
 cleanData <- data[(! is.na(data$Global_active_power)) & (! is.na(data$Global_reactive_power)) & (! is.na(data$Global_intensity)) & (! is.na(data$Voltage)) & (! is.na(data$Sub_metering_1)) & (! is.na(data$Sub_metering_2)) & (! is.na(data$Sub_metering_3)),]
 usefulData <- cleanData[as.Date(cleanData$Date,format='%d/%m/%Y') == as.Date('2007-02-01') | as.Date(cleanData$Date,format='%d/%m/%Y') == as.Date('2007-02-02'),]
-png("plot1.png", width = 480, height = 480)
-hist(usefulData$Global_active_power,col = 'red',xlab = 'Global Active Power (kilowatts)',main = 'Global Active Power')
-title(main = "Global Active Power")
+png("plot2.png", width = 480, height = 480)
+plot(usefulData$Time ,usefulData$Global_active_power  ,ylab = 'Global Active Power (kilowatts)',xlab ='',type = 'l')
 dev.off()
